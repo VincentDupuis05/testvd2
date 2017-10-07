139fdce6146f1cabaac373157e4c75964424ee35
vincent <vincent.dupuis05@free.fr>
2017-10-07 18:24:55 +0200

    initial commit

---
+++ User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+// Le commentaire 1
+// nouv comment vd 106_01
+
+// nouv comment vd 106_02
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+    // Le commentaire 2
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
+
+modif 3
